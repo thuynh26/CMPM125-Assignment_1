880c3c9e5c3fd0f14286fc3898901a65ed400b97
thuynh26 <thuynh26@ucsc.edu>
2026-04-10 13:01:17 -0700

    Added simple movement input to vehicle

---
+++ CMPM125-Assignment1/Assets/Scripts/VehicleController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VehicleController : MonoBehaviour
+{
+    public float desired_acceleration;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(desired_acceleration*10, 0, 0);
+    }
+
+    void OnMove(InputValue action)
+    {
+        var movement = action.Get<Vector2>();
+        desired_acceleration = movement.y;
+    }
+}
